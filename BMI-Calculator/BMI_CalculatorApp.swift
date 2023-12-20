c376731119bc412024e05d8c4abf4f9404bc861a
İsmail KURT <ismail.kurtf@gmail.com>
2023-12-20 23:20:26 +0300

    Initial Commit

---
+++ BMI-Calculator/BMI_CalculatorApp.swift
@@ -0,0 +1,20 @@
+//
+//  BMI_CalculatorApp.swift
+//  BMI-Calculator
+//
+//  Created by İsmail KURT on 20.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BMI_CalculatorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
