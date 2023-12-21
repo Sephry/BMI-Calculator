062c379e4e5bccaa1210171f2fda58daf5f8c3bd
Alperen Aydın <alperen.aydn16@hotmail.com>
2023-12-22 01:25:32 +0300

    onboardingrootview - düzenlenme yapılacak

--- BMI-Calculator/Views/OnboardingViews/OnboardingRootView.swift
+++ BMI-Calculator/Views/OnboardingViews/OnboardingRootView.swift
@@ -1,29 +1,70 @@
-//
-//  OnboardingRootView.swift
-//  BMI-Calculator
-//
-//  Created by İsmail KURT on 21.12.2023.
-//
-
 import SwiftUI
 
 struct OnboardingRootView: View {
 
     @Binding var userName: String
-    
+
     var body: some View {
-        VStack{
-            TextField("Enter your Name", text: $userName)
-            Button{
-                userName = "Alperen Sik"
-                UserDefaults.standard.set(false, forKey: "isFirstTimeLaunch")
-            }label: {
-                Text("Press")
+        
+        ZStack {
+            
+            LinearGradient(gradient: Gradient(colors: [.green, .teal]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+
+            VStack {
+                Spacer()
+                Image("hi")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .padding(.bottom, 20)
+
+                Text("Merhaba")
+                    .font(.title)
+                    .foregroundColor(.white)
+
+              Spacer()
+                
+                ZStack {
+                    Rectangle()
+                        .fill(  LinearGradient(gradient: Gradient(colors: [.white, .gray]), startPoint: .bottom, endPoint: .top))
+                        .frame(height: 300)
+                        .cornerRadius(20)
+                        .padding(.top, 20)
+
+                    VStack {
+                        Text("What's your name?")
+                            .foregroundColor(.black)
+                            .padding()
+
+                        TextField("Enter your Name", text: $userName)
+                            .textFieldStyle(CustomTextFieldStyle(borderColor: .white, backgroundColor: .white))
+                            .foregroundColor(.black)
+                            .padding()
+
+                        Button(action: {
+                            print("Button tapped!")
+                        }) {
+                            Spacer()
+                            Text("Next")
+                                .padding()
+                                .background(Color.color2)
+                                .foregroundColor(.color1)
+                        }
+                        .cornerRadius(10)
+                        .padding(.top, 20)
+                    }
+                    .padding()
+                    .foregroundColor(.white)
+                }.ignoresSafeArea()
+                .frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .bottom)
             }
         }
     }
 }
 
-#Preview {
-    OnboardingRootView(userName: .constant("ismail"))
+struct OnboardingRootView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingRootView(userName: .constant("ismail"))
+    }
 }
