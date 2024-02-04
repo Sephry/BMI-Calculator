f7c65ecbbde4d7296706eb5c953ba7b8f1154949
İsmail KURT <ismail.kurtf@gmail.com>
2024-02-04 16:20:19 +0300

    paywall screen and target data fetch request

---
+++ BMI-Calculator/Views/PaywallView.swift
@@ -0,0 +1,18 @@
+//
+//  PaywallView.swift
+//  BMI-Calculator
+//
+//  Created by İsmail KURT on 4.02.2024.
+//
+
+import SwiftUI
+
+struct PaywallView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaywallView()
+}
