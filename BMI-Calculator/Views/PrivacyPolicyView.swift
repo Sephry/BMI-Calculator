116dbaeca134d0dd83f6bbeab755431a4c740cd2
Alperen Aydın <alperen.aydn16@hotmail.com>
2024-02-16 19:17:10 +0300

    PrivacyPolicyView eklendi ve ProfileView düzenlendi.

--- BMI-Calculator/Views/PrivacyPolicyView.swift
+++ BMI-Calculator/Views/PrivacyPolicyView.swift
@@ -1,18 +1,47 @@
-//
-//  PrivacyPolicyView.swift
-//  BMI-Calculator
-//
-//  Created by Alperen Aydın on 16.02.2024.
-//
-
 import SwiftUI
 
 struct PrivacyPolicyView: View {
     var body: some View {
-        Text("")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.PrimaryColor, Color.SecondaryColor]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Gizlilik Politikası")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                    Text("Toplanan Bilgiler")
+                        .foregroundColor(.white)
+                    Text("Uygulama, kullanıcıların kilo ve boy gibi kişisel bilgileri girmesini sağlayabilir. Bu bilgiler, kullanıcının vücut kitle indeksi (BMI) hesaplanması için kullanılır. Uygulama, kişisel bilgileri cihazda yerel olarak saklar ve herhangi bir uzak sunucuya iletilmez.")
+                    
+                    Text("Bilgilerin Kullanımı")
+                        .foregroundColor(.white)
+                    Text("Toplanan bilgiler, kullanıcının vücut kitle indeksini (BMI) hesaplamak için kullanılır. Bu bilgiler, kullanıcının sağlığına ilişkin genel bir değerlendirme yapmak amacıyla kullanılır ve herhangi bir kişisel kimlik belirtisi içermez.")
+                    
+                    Text("Bilgilerin Paylaşılması")
+                        .foregroundColor(.white)
+                    Text("Uygulama tarafından toplanan kişisel bilgiler, üçüncü taraflarla paylaşılmaz veya satılmaz.")
+                    Text("Çocukların Gizliliği")
+                        .foregroundColor(.white)
+                    Text("Uygulamamız 13 yaşın altındaki kullanıcılar için değildir. Bilerek çocukların kişisel bilgilerini toplamıyoruz. Ebeveynlerin, çocuklarının internet kullanımını denetlemesi ve çevrimiçi gizliliklerini sağlaması önemlidir.")
+                    Text("Değişiklikler")
+                        .foregroundColor(.white)
+                    Text("Bu gizlilik politikası, uygulamamızdaki değişiklikler ve güncellemelerle birlikte gözden geçirilebilir. Yeni bir güncelleme yayınlandığında, bu sayfada güncellenmiş bir sürümünü yayınlarız.")
+                    Text("İletişim")
+                        .foregroundColor(.white)
+                    Text("Gizlilik politikamızla ilgili herhangi bir sorunuz veya endişeniz varsa, lütfen bizimle iletişime geçmekten çekinmeyin: ismo.kurt1234@gmail.com - alperen.aydn16@hotmail.com")
+                }
+                .padding()
+            }
+        }
     }
 }
 
-#Preview {
-    PrivacyPolicyView()
+struct PrivacyPolicyView_Previews: PreviewProvider {
+    static var previews: some View {
+        PrivacyPolicyView()
+    }
 }
--- BMI-Calculator/Views/ProfileView.swift
+++ BMI-Calculator/Views/ProfileView.swift
@@ -9,62 +9,66 @@ import SwiftUI
 
 struct ProfileView: View {
     @Binding var currentTab: Int
-
     @AppStorage("isDarkModeEnabled") var isDarkModeEnabled: Bool = false
-
     @State var downloadViaWifiEnabled: Bool = false
-    
     let userName: String
+    @State private var isPrivacyPolicyPresented = false // State değişkeni, PrivacyPolicyView'in açılıp kapanmasını kontrol eder
     
     var body: some View {
-                    Form {
-                        Group {
-                            HStack{
-                                Spacer()
-                                VStack {
-                                    Image(systemName: "person")
-                                        .resizable()
-                                        .frame(width:100, height: 100, alignment: .center)
-                                        .padding()
-                                    Text(userName)
-                                        .font(.title)
-                                }
-                                Spacer()
-                            }
-                        }
-                        
-                        Section(header: Text("CONTENT"), content: {
-                            HStack{
-                                
-                                ShareLink(item: URL(string: "https://apps.apple.com/us/app/light-speedometer/id6447198696")!) {
-                                            Label("Share", systemImage: "square.and.arrow.up")
-                                        }
-                            }
-
-                            HStack{
-                                Image(systemName: "lock.shield")
-                                Text("Privacy Policy")
-                            }
-
-                        })
-
-                        Section(header: Text("PREFRENCES"), content: {
-                            HStack{
-                                Image(systemName: "network")
-                                Text("Language")
-                            }
-                            HStack{
-                                Image(systemName: "moon")
-                                Toggle(isOn: $isDarkModeEnabled) {
-                                    Text("Dark Mode")
-                                }
-                            }
-
-                        })
+        Form {
+            Group {
+                HStack {
+                    Spacer()
+                    VStack {
+                        Image(systemName: "person")
+                            .resizable()
+                            .frame(width:100, height: 100, alignment: .center)
+                            .padding()
+                        Text(userName)
+                            .font(.title)
                     }
-                
+                    Spacer()
+                }
+            }
+            
+            Section(header: Text("CONTENT")) {
+                HStack {
+                    ShareLink(item: URL(string: "https://apps.apple.com/us/app/light-speedometer/id6447198696")!) {
+                        Label("Share", systemImage: "square.and.arrow.up")
+                    }
+                }
+                HStack {
+                    Button(action: {
+                        isPrivacyPolicyPresented.toggle() // PrivacyPolicyView'i açar
+                    }) {
+                        HStack {
+                            Image(systemName: "lock.shield")
+                            Text("Privacy Policy")
+                        }
+
+                    }
+                    .sheet(isPresented: $isPrivacyPolicyPresented) {
+                        PrivacyPolicyView()
+                    }
+                }
+            }
+            
+            Section(header: Text("PREFERENCES")) {
+                HStack {
+                    Image(systemName: "network")
+                    Text("Language")
+                }
+                HStack {
+                    Image(systemName: "moon")
+                    Toggle(isOn: $isDarkModeEnabled) {
+                        Text("Dark Mode")
+                    }
+                }
+            }
+        }
     }
 }
+
 
 #Preview {
     ProfileView(currentTab: .constant(1), userName: "ismail")
