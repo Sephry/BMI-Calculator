d0b4c33f5deae1829477f7f2149f26a79f24dc46
Alperen Aydın <alperen.aydn16@hotmail.com>
2024-02-16 19:15:05 +0300

    PrivacyPolicyView eklendi ve ProfileView düzenlendi

---
+++ BMI-Calculator/Views/PrivacyPolicyView.swift
@@ -0,0 +1,18 @@
+//
+//  PrivacyPolicyView.swift
+//  BMI-Calculator
+//
+//  Created by Alperen Aydın on 16.02.2024.
+//
+
+import SwiftUI
+
+struct PrivacyPolicyView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    PrivacyPolicyView()
+}
